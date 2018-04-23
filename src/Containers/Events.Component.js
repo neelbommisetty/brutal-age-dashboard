a00fac1e8504b8af6f81310cab30122252df4bb3
Neel Bommisetty <neel250294@gmail.com>
2018-04-24 01:35:52 +0530

    calendar updates

--- src/Containers/Events.Component.js
+++ src/Containers/Events.Component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Message } from 'semantic-ui-react';
 import './events.css';
 
 export default class EventsComponent extends Component {
@@ -13,14 +14,22 @@ export default class EventsComponent extends Component {
 
   render() {
     return (
-      <div className="google-calendar">
-        <iframe
-          title="calendar"
-          src="https://calendar.google.com/calendar/embed?src=9rhi75tbrcms4l9836lcikn50g%40group.calendar.google.com"
-          style={{ border: 0 }}
-          width="100%"
-          height="600"
-        />
+      <div>
+        <Message>
+          <Message.Header>Daylight Savings</Message.Header>
+          The events might be off by 1 hour for few timezones especially in
+          countries that do not Observe day light savings like India/China Etc.
+        </Message>
+        {/* <iframe src="https://calendar.google.com/calendar/embed?showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=9rhi75tbrcms4l9836lcikn50g%40group.calendar.google.com&amp;color=%23B1440E&amp;ctz=Asia%2FCalcutta" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe> */}
+        <div className="google-calendar">
+          <iframe
+            title="calendar"
+            src="https://calendar.google.com/calendar/embed?src=9rhi75tbrcms4l9836lcikn50g%40group.calendar.google.com&amp;showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;mode=AGENDA"
+            style={{ border: 0 }}
+            width="100%"
+            height="600"
+          />
+        </div>
       </div>
     );
   }
