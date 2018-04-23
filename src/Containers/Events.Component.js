40b0f83c3518ec51f0b3dfc4519b4c152f23f6c5
Neel Bommisetty <neel250294@gmail.com>
2018-04-24 01:39:58 +0530

    credits and final changes

--- src/Containers/Events.Component.js
+++ src/Containers/Events.Component.js
@@ -20,6 +20,18 @@ export default class EventsComponent extends Component {
           The events might be off by 1 hour for few timezones especially in
           countries that do not Observe day light savings like India/China Etc.
         </Message>
+        <Message>
+          <Message.Header>Credits</Message.Header>
+          All the Events data is gathered by{' '}
+          <a
+            href="https://www.facebook.com/momattheword"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Matthew Ord.
+          </a>
+          &nbsp;All Credit Goes to him.
+        </Message>
         {/* <iframe src="https://calendar.google.com/calendar/embed?showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=9rhi75tbrcms4l9836lcikn50g%40group.calendar.google.com&amp;color=%23B1440E&amp;ctz=Asia%2FCalcutta" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe> */}
         <div className="google-calendar">
           <iframe
